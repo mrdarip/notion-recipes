804be137d8cf25d43b75c8f0614e85f524a8143c
mrdarip <darioelas2005@gmail.com>
2024-08-19 16:57:07 +0200

    refactored DAO's and implemented queryDatabase

--- js/NotionDAOs/DAO.js
+++ js/NotionDAOs/DAO.js
@@ -1,4 +1,4 @@
-class MealDAO{
+class DAO{
     static queryRecipesOf(id){
         console.log("Querying recipes of meal with id: " + id);
         console.error("Notion API is not implemented yet");
