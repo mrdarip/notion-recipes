c32799dd1e25b464cced05cbc86752f4f04fd02e
mrdarip <darioelas2005@gmail.com>
2024-08-19 16:14:16 +0200

    implemented DAOs

---
+++ js/NotionDAO.js
@@ -0,0 +1,11 @@
+class NotionDAO {
+    constructor(secret) {
+        this.secret = secret;
+    }
+
+    queryDatabase(databaseId) {
+        console.log("Querying database with id: " + databaseId);
+
+        console.error("Notion API is not implemented yet");
+    }
+}
