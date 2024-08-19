7b5475e48d82fb9c7ecad3eb5d9836d5fe8bdc69
mrdarip <darioelas2005@gmail.com>
2024-08-19 18:46:00 +0200

    implemented DAO methods

---
+++ js/DAO.js
@@ -0,0 +1,26 @@
+class DAO{
+    static queryIngredientsOf(id){
+        console.log("Querying ingredients of meal with id: " + id);
+        console.error("Not implemented yet");
+    }
+
+    static queryMeals(){
+        console.log("Querying all meals");
+        console.error("Not implemented yet");
+    }
+
+    static queryMeal(id){
+        console.log("Querying meal with id: " + id);
+        console.error("Not implemented yet");
+    }
+
+    static queryIngredients(){
+        console.log("Querying all ingredients");
+        console.error("Not implemented yet");
+    }
+
+    static queryIngredient(id){
+        console.log("Querying ingredient with id: " + id);
+        console.error("Not implemented yet");
+    }
+}
