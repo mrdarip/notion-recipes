42f9b7224a7118658633960107751dd8ae0f7c7d
christdc <cristinadiazcabello@gmail.com>
2024-08-22 23:50:57 +0200

    added general.css

--- js/headerfooter.js
+++ js/headerfooter.js
@@ -2,6 +2,6 @@ window.onload = function ()
     {
         var header = document.getElementById('header');
 
-        header.innerHTML = "<nav><ul><li><a href=\"http://127.0.0.1:3000/index.html\"><i class=\"fa-solid fa-house\"></i></a></li><li><input placeholder=\"Search...\" type=\"text\" id=\"searchText\"></li><li><a href=\"#\" id=\"createLink\">Create</a></li><li><a href=\"#\" id=\"manageLink\">Manage</a></li></ul></nav>";
+        header.innerHTML = "<nav><ul><li><a href=\"http://127.0.0.1:3000/index.html\"><i class=\"fa-solid fa-house\"></i></a></li><li><input placeholder=\"Search...\" type=\"text\" id=\"searchText\"></li><li><a href=\"http://127.0.0.1:3000/html/create.html\" id=\"createLink\">Create</a></li><li><a href=\"http://127.0.0.1:3000/html/manage.html\" id=\"manageLink\">Manage</a></li></ul></nav>";
         
     }
