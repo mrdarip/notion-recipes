b3e18607bf0fd7bee5baf65c7e4e16c92f7a66f4
mrdarip <darioelas2005@gmail.com>
2024-08-31 22:12:34 +0200

    added actual commit pages

--- js/commit.js
+++ js/commit.js
@@ -1,19 +1,16 @@
 var output = document.getElementById('output');
 var anchor = document.getElementById('commit-anchor');
 
-
-anchor.setAttribute("href", "index.html");
-console.log(anchor);
-
-
 var url = new URL(window.location.href);
 var ingredient = url.searchParams.get("ingredient");
 var meal = url.searchParams.get("meal");
 
 if (ingredient != null) {
     output.innerHTML = dao.getIngredients() + ingredient;
+    anchor.href = "https://github.com/mrdarip/notion-recipes/edit/main/data/ingredients.json";
 }
 
 if (meal != null) {
     output.innerHTML = dao.getMeals() + meal;
+    anchor.href = "https://github.com/mrdarip/notion-recipes/edit/main/data/meals.json";
 }
